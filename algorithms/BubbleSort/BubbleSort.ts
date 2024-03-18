dd23984d2d2c7949ac19b0c59b232de9dd1e7bdb
Rafael Villarreal <rvillarreal416@gmail.com>
2024-03-18 13:58:23 -0500

    feat: add bubble sort

---
+++ algorithms/BubbleSort/BubbleSort.ts
@@ -0,0 +1,11 @@
+export default function bubble_sort(array: number[]): void {
+  const n = array.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+}
